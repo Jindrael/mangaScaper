ee266ebc660811eacfce43dae06880cb43a65ec9
Jin <Jin@local-macbook>
2018-04-18 20:09:30 +0200

    - first commit test with IntelliJ

---
+++ src/main.java
@@ -0,0 +1,7 @@
+public class main {
+
+    public static void main(String[] args) {
+        String test = "This is a test String, just to see if that is all working!";
+        System.out.println(test);
+    }
+}
